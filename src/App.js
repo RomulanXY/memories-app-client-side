102656189450cb10d786cb4b0cbe3992a1e28d30
Oluwaseyi Folahan Obadeyi <50980172+RomulanXY@users.noreply.github.com>
2022-07-06 14:49:08 +0100

    feat: Form use was introduced

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./images/memories.png";
 
 const App = ()=>{
     return(
-        <div>
-            <h1>Hello!</h1>
-        </div>
+        <Container maxwidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={ memories } alt="memories" height="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}></Grid>
+
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
